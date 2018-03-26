807b109035ccb9c415e1f14823935b1af21e9993
Rjected <kidscline01@gmail.com>
2018-03-26 18:48:36 -0400

    added StackItem to InteropService

--- src/InteropService.ex
+++ src/InteropService.ex
@@ -1,10 +1,40 @@
-require Mixins.EquatableMixin
+import Mixins
 
 defmodule CollectionMixin do
     
-  isSynchronized = false;
-  syncRoot = nil;
+  isSynchronized = false
+  syncRoot = nil
 
   @count 0
+
+  def contains(self, item) do
+
+  end
+
+  def clear(self) do
+
+  end
+
+  def copyTo(self, array, index) do
+
+  end
+
+end
+
+defmodule StackItem do
+
+  @isStruct false
+
+  @hash fn(self) ->
+    hash = 17
+
+    for b <- self.getByteArray do
+      hash = hash * 31 + b
+    end
+
+    hash
+
+  end
+
 
 end
