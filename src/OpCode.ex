2e38365475023b9a5ea21a01647da0966da14204
BSathvik <sathvikb30@gmail.com>
2018-03-26 22:46:40 -0400

    OpCode done and tested

--- src/OpCode.ex
+++ src/OpCode.ex
@@ -1,9 +1,9 @@
 defmodule OpCode do
-  #  constants,
+  #  constants
  @all_att %{
-   :push0 => "\x00",  # an empty array of bytes is pushed onto the stack.,
+   :push0 => "\x00",  # an empty array of bytes is pushed onto the stack.
    :pushf => "\x00", #NOTE
-   :pushbytes1 => "\x01",  # b"\x01-b"\x4b the next opcode bytes is data to be pushed onto the stack,
+   :pushbytes1 => "\x01",  # b"\x01-b"\x4b the next opcode bytes is data to be pushed onto the stack.
    :pushbytes2 => "\x02",
    :pushbytes3 => "\x03",
    :pushbytes4 => "\x04",
@@ -79,30 +79,30 @@ defmodule OpCode do
    :pushbytes74 => "\x4a",
    :pushbytes75 => "\x4b",
    :pushdata1 => "\x4c",  # the next byte contains the number of bytes to be pushed onto the stack.
-   :pushdata2 => "\x4d",  # the next two bytes contain the number of bytes to be pushed onto the stack.,
-   :pushdata4 => "\x4e",  # the next four bytes contain the number of bytes to be pushed onto the stack.,
-   :pushm1 => "\x4f",  # the number -1 is pushed onto the stack.,
-   :push1 => "\x51",  # the number 1 is pushed onto the stack.,
+   :pushdata2 => "\x4d",  # the next two bytes contain the number of bytes to be pushed onto the stack.
+   :pushdata4 => "\x4e",  # the next four bytes contain the number of bytes to be pushed onto the stack.
+   :pushm1 => "\x4f",  # the number -1 is pushed onto the stack.
+   :push1 => "\x51",  # the number 1 is pushed onto the stack.
    :pusht => "\x51", #NOTE
-   :push2 => "\x52",  # the number 2 is pushed onto the stack.,
-   :push3 => "\x53",  # the number 3 is pushed onto the stack.,
-   :push4 => "\x54",  # the number 4 is pushed onto the stack.,
-   :push5 => "\x55",  # the number 5 is pushed onto the stack.,
-   :push6 => "\x56",  # the number 6 is pushed onto the stack.,
-   :push7 => "\x57",  # the number 7 is pushed onto the stack.,
-   :push8 => "\x58",  # the number 8 is pushed onto the stack.,
-   :push9 => "\x59",  # the number 9 is pushed onto the stack.,
-   :push10 => "\x5a",  # the number 10 is pushed onto the stack.,
-   :push11 => "\x5b",  # the number 11 is pushed onto the stack.,
-   :push12 => "\x5c",  # the number 12 is pushed onto the stack.,
-   :push13 => "\x5d",  # the number 13 is pushed onto the stack.,
-   :push14 => "\x5e",  # the number 14 is pushed onto the stack.,
-   :push15 => "\x5f",  # the number 15 is pushed onto the stack.,
-   :push16 => "\x60",  # the number 16 is pushed onto the stack.,
+   :push2 => "\x52",  # the number 2 is pushed onto the stack.
+   :push3 => "\x53",  # the number 3 is pushed onto the stack.
+   :push4 => "\x54",  # the number 4 is pushed onto the stack.
+   :push5 => "\x55",  # the number 5 is pushed onto the stack.
+   :push6 => "\x56",  # the number 6 is pushed onto the stack.
+   :push7 => "\x57",  # the number 7 is pushed onto the stack.
+   :push8 => "\x58",  # the number 8 is pushed onto the stack.
+   :push9 => "\x59",  # the number 9 is pushed onto the stack.
+   :push10 => "\x5a",  # the number 10 is pushed onto the stack.
+   :push11 => "\x5b",  # the number 11 is pushed onto the stack.
+   :push12 => "\x5c",  # the number 12 is pushed onto the stack.
+   :push13 => "\x5d",  # the number 13 is pushed onto the stack.
+   :push14 => "\x5e",  # the number 14 is pushed onto the stack.
+   :push15 => "\x5f",  # the number 15 is pushed onto the stack.
+   :push16 => "\x60",  # the number 16 is pushed onto the stack.
 
 
     #  flow control,
-   :nop => "\x61",  # does nothing.,
+   :nop => "\x61",  # does nothing.
    :jmp => "\x62",
    :jmpif => "\x63",
    :jmpifnot => "\x64",
@@ -128,13 +128,13 @@ defmodule OpCode do
    :depth => "\x74",  # puts the number of stack items onto the stack.
    :drop => "\x75",  # removes the top stack item.
    :dup => "\x76",  # duplicates the top stack item.
-   :nip => "\x77",  # removes the second-to-top stack item.,
-   :over => "\x78",  # copies the second-to-top stack item to the top.,
-   :pick => "\x79",  # the item n back in the stack is copied to the top.,
-   :roll => "\x7a",  # the item n back in the stack is moved to the top.,
-   :rot => "\x7b",  # the top three items on the stack are rotated to the left.,
-   :swap => "\x7c",  # the top two items on the stack are swapped.,
-   :tuck => "\x7d",  # the item at the top of the stack is copied and inserted before the second-to-top item.,
+   :nip => "\x77",  # removes the second-to-top stack item.
+   :over => "\x78",  # copies the second-to-top stack item to the top.
+   :pick => "\x79",  # the item n back in the stack is copied to the top.
+   :roll => "\x7a",  # the item n back in the stack is moved to the top.
+   :rot => "\x7b",  # the top three items on the stack are rotated to the left.
+   :swap => "\x7c",  # the top two items on the stack are swapped.
+   :tuck => "\x7d",  # the item at the top of the stack is copied and inserted before the second-to-top item.
 
 
     #  splice,
@@ -146,61 +146,61 @@ defmodule OpCode do
 
 
     #  bitwise logic,
-   :invert => "\x83",  # flips all of the bits in the input.,
-   :and_ => "\x84",  # boolean and between each bit in the inputs. #NOTE: and is reserved so => "and_",
+   :invert => "\x83",  # flips all of the bits in the input.
+   :and_ => "\x84",  # boolean and between each bit in the inputs. #NOTE: and is reserved so => "and_"
    :or_ => "\x85",  # boolean or between each bit in the inputs. #NOTE: or is reserved so => "or_",
-   :xor => "\x86",  # boolean exclusive or between each bit in the inputs.,
-   :equal => "\x87",  # returns 1 if the inputs are exactly equal" 0 otherwise.,
-    # op_equalverify => "\x88" #  same as op_equal" but runs op_verify afterward.,
-    # op_reserved1 => "\x89" #  transaction is invalid unless occuring in an unexecuted op_if branch,
-    # op_reserved2 => "\x8a" #  transaction is invalid unless occuring in an unexecuted op_if branch,
+   :xor => "\x86",  # boolean exclusive or between each bit in the inputs.
+   :equal => "\x87",  # returns 1 if the inputs are exactly equal" 0 otherwise.
+    # op_equalverify => "\x88" #  same as op_equal" but runs op_verify afterward.
+    # op_reserved1 => "\x89" #  transaction is invalid unless occuring in an unexecuted op_if branch
+    # op_reserved2 => "\x8a" #  transaction is invalid unless occuring in an unexecuted op_if branch
 
     #  arithmetic,
-    #  note: arithmetic inputs are limited to signed 32-bit integers" but may overflow their output.,
-   :inc => "\x8b",  # 1 is added to the input.,
-   :dec => "\x8c",  # 1 is subtracted from the input.,
+    #  note: arithmetic inputs are limited to signed 32-bit integers" but may overflow their output.
+   :inc => "\x8b",  # 1 is added to the input.
+   :dec => "\x8c",  # 1 is subtracted from the input.
    :sign => "\x8d",
-   :negate => "\x8f",  # the sign of the input is flipped.,
+   :negate => "\x8f",  # the sign of the input is flipped.
    :abs => "\x90",  # the input is made positive.,
-   :not => "\x91",  # if the input is 0 or 1" it is flipped. otherwise the output will be 0.,
-   :nz => "\x92",  # returns 0 if the input is 0. 1 otherwise.,
-   :add => "\x93",  # a is added to b.,
-   :sub => "\x94",  # b is subtracted from a.,
-   :mul => "\x95",  # a is multiplied by b.,
-   :div => "\x96",  # a is divided by b.,
-   :mod => "\x97",  # returns the remainder after dividing a by b.,
-   :shl => "\x98",  # shifts a left b bits" preserving sign.,
-   :shr => "\x99",  # shifts a right b bits" preserving sign.,
-   :booland => "\x9a",  # if both a and b are not 0" the output is 1. otherwise 0.,
-   :boolor => "\x9b",  # if a or b is not 0" the output is 1. otherwise 0.,
-   :numequal => "\x9c",  # returns 1 if the numbers are equal" 0 otherwise.,
-   :numnotequal => "\x9e",  # returns 1 if the numbers are not equal" 0 otherwise.,
-   :lt => "\x9f",  # returns 1 if a is less than b" 0 otherwise.,
-   :gt => "\xa0",  # returns 1 if a is greater than b" 0 otherwise.,
-   :lte => "\xa1",  # returns 1 if a is less than or equal to b" 0 otherwise.,
-   :gte => "\xa2",  # returns 1 if a is greater than or equal to b" 0 otherwise.,
-   :min => "\xa3",  # returns the smaller of a and b.,
-   :max => "\xa4",  # returns the larger of a and b.,
-   :within => "\xa5",  # returns 1 if x is within the specified range (left-inclusive)" 0 otherwise.,
+   :not => "\x91",  # if the input is 0 or 1" it is flipped. otherwise the output will be 0.
+   :nz => "\x92",  # returns 0 if the input is 0. 1 otherwise.
+   :add => "\x93",  # a is added to b.
+   :sub => "\x94",  # b is subtracted from a.
+   :mul => "\x95",  # a is multiplied by b.
+   :div => "\x96",  # a is divided by b.
+   :mod => "\x97",  # returns the remainder after dividing a by b.
+   :shl => "\x98",  # shifts a left b bits" preserving sign.
+   :shr => "\x99",  # shifts a right b bits" preserving sign.
+   :booland => "\x9a",  # if both a and b are not 0" the output is 1. otherwise 0.
+   :boolor => "\x9b",  # if a or b is not 0" the output is 1. otherwise 0.
+   :numequal => "\x9c",  # returns 1 if the numbers are equal" 0 otherwise.
+   :numnotequal => "\x9e",  # returns 1 if the numbers are not equal" 0 otherwise.
+   :lt => "\x9f",  # returns 1 if a is less than b" 0 otherwise.
+   :gt => "\xa0",  # returns 1 if a is greater than b" 0 otherwise.
+   :lte => "\xa1",  # returns 1 if a is less than or equal to b" 0 otherwise.
+   :gte => "\xa2",  # returns 1 if a is greater than or equal to b" 0 otherwise.
+   :min => "\xa3",  # returns the smaller of a and b.
+   :max => "\xa4",  # returns the larger of a and b.
+   :within => "\xa5",  # returns 1 if x is within the specified range (left-inclusive)" 0 otherwise.
 
-    #  crypto,
-    # ripemd160 => "\xa6" #  the input is hashed using ripemd-160.,
-   :sha1 => "\xa7",  # the input is hashed using sha-1.,
-   :sha256 => "\xa8",  # the input is hashed using sha-256.,
+    #  Crypto
+    # ripemd160 => "\xa6" #  the input is hashed using ripemd-160.
+   :sha1 => "\xa7",  # the input is hashed using SHA-1
+   :sha256 => "\xa8",  # the input is hashed using SHA-256.
    :hash160 => "\xa9",
    :hash256 => "\xaa",
    :checksig => "\xac",
    :checkmultisig => "\xae",
 
 
-    #  array,
+    #  Array,
    :arraysize => "\xc0",
    :pack => "\xc1",
    :unpack => "\xc2",
    :pickitem => "\xc3",
    :setitem => "\xc4",
-   :newarray => "\xc5",  # 用作引用類型,
-   :newstruct => "\xc6",  # 用作值類型,
+   :newarray => "\xc5",  # 用作引用類型
+   :newstruct => "\xc6",  # 用作值類型
    :append => "\xc8",
    :reverse => "\xc9",
    :remove => "\xca",
@@ -209,12 +209,8 @@ defmodule OpCode do
 
   }
 
-  def to_name do
-
-    op_codes = Map.keys(@all_att)
-
-    
-
+  def get_opcode(op) do
+    @all_att[op]
   end
 
 end
