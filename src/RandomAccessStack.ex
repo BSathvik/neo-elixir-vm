7006e09f46233621682896c84c0f0a8b568f1034
Rjected <kidscline01@gmail.com>
2018-04-06 19:19:32 -0400

    Finished RandomAccessStack

--- src/RandomAccessStack.ex
+++ src/RandomAccessStack.ex
@@ -3,26 +3,53 @@ defmodule RandomAccessStack do
   defstruct list: []
 
   def new, do: %RandomAccessStack{}
-  
-  def push(stack, element), do: %RandomAccessStack{stack | list: [element] ++ stack.list}
 
-  def pop(%RandomAccessStack{list: []}), do: raise "RandomAccessStack is empty"
+  def items(%RandomAccessStack{list: list}), do: list
 
-  def pop(%RandomAccessStack{list: [top | rest]}), do: {top, %RandomAccessStack{list: rest}}
-
-  def size(%RandomAccessStack{list: list}), do: list |> length
+  def count(%RandomAccessStack{list: list}), do: list |> length
 
   def peek(%RandomAccessStack{list: []}), do: raise "RandomAccessStack is empty"
 
   def peek(%RandomAccessStack{list: [top | _]}), do: top
 
-  def insert(%RandomAccessStack{list: list}, index, item) do
-    %RandomAccessStack{list: list |> List.insert_at(index, item)}
+  def pop(%RandomAccessStack{list: []}), do: raise "RandomAccessStack is empty"
+
+  def pop(%RandomAccessStack{list: [top | rest]}) do
+
+    {top, %RandomAccessStack{list: rest}}
+
   end
 
-  def clear(%RandomAccessStack{list: list}), do: %RandomAccessStack{list: []}
+  def push(stack, element) do
+
+    %RandomAccessStack{stack | list: [element] ++ stack.list}
+
+  end
+
+  def insert(%RandomAccessStack{list: list}, index, item) do
+
+    %RandomAccessStack{list: list |> List.insert_at(index, item)}
+
+  end
+
+  def clear(%RandomAccessStack{}) do
+
+    %RandomAccessStack{list: []}
+
+  end
 
   def set(%RandomAccessStack{list: list}, index, item) do
+
     %RandomAccessStack{list: list |> List.replace_at(index, item)}
+
   end
+
+  def remove(%RandomAccessStack{list: list}, index) do
+
+    list |> List.pop_at(index) |> (fn ({head , tail}) ->
+      {head, %RandomAccessStack{list: tail}}
+    end).()
+
+  end
+
 end
